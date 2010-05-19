29438fc51ca8a263be94673c809a983998bc9060
Tom Swirly <tom@swirly.com>
2010-05-18 22:08:21 -0400

    Added WriteJSON.js

---
+++ js/swirly/live/setter.js
@@ -0,0 +1,27 @@
+#ifndef __STATE_SETTER
+#define __STATE_SETTER
+
+#include "swirly/live/live.js"
+#include "swirly/live/translate_path.js"
+
+Live.Setter = function(command) {
+  return function() {
+    var path = command.slice();
+    var value = path.pop();
+    var valueName = path.pop();
+    var fullPath = Live.translatePath(path);
+
+    if (!fullPath) {
+      post('Unable to apply a function to path', path, '\n');
+      return function() {};
+    }
+
+    Live.api.path = fullPath;
+    var undo = command.slice();
+    undo[undo.length - 1] = Live.api.get(valueName);
+    Live.api.set(valueName, value);
+    return Live.Setter(undo);
+  };
+};
+
+#endif  // __STATE_SETTER
--- js/tests.js
+++ js/tests.js
@@ -53,6 +53,7 @@ autowatch = 1;
 // Unit tests start here.  Each subdirectory has a file called "tests.js" that
 // lists all the active tests in that directory.
 #include "swirly/array/tests.js"
+#include "swirly/live/tests.js"
 #include "swirly/undo/tests.js"
 #include "swirly/util/tests.js"
 
