b1cea330e3112f35e8813072a187edd1918b913a
Tom Swirly <tom@swirly.com>
2010-05-19 12:58:50 -0400

    New Array.ConcatSafe.

--- js/swirly/array/concat.js
+++ js/swirly/array/concat.js
@@ -2,7 +2,11 @@
 #define __ARRAY_CONCAT
 
 // Concatinate two arguments or argument lists, only returning a new list if
-// both lists are non-empty.
+// both lists are non-empty.  My current thinking on this code is that it's a
+// tiny bit faster, but less clear and less safe than simply creating a new list
+// with Array.ConcatSafe and I'll probably get rid of this as a result.  (It's
+// unsafe because if you modify the resturned result, you might be modifying one
+// of the original arrays...!)
 Array.Concat = function(x, y) {
   if (!(x && x.length))
     return y;
@@ -10,6 +14,12 @@ Array.Concat = function(x, y) {
   if (!(y && y.length))
     return x;
 
+  return Array.ConcatSafe(x, y);
+};
+
+Array.ConcatSafe = function(x, y) {
+  // If x doesn't have the .concat method, it's an argument list: see
+  // http://ax.to/jsarguments
   if (!x.concat)
     x = arrayfromargs(x);
 
--- js/swirly/array/concat_test.js
+++ js/swirly/array/concat_test.js
@@ -1,6 +1,8 @@
 #include "swirly/array/concat.js"
 
 Testing.TestFunction(function() {
+  // getArgs() returns an argument list from its arguments,
+  // as in http://ax.to/jsarguments.
   function getArgs(_) { return arguments; }
 
   Testing.ExpectFunction('Array.concat', Array.Concat, this,
@@ -10,6 +12,15 @@ Testing.TestFunction(function() {
   Testing.ExpectFunction('Array.concat', Array.Concat, this,
                          [getArgs(1, 2), getArgs()], getArgs(1, 2));
   Testing.ExpectFunction('Array.concat', Array.Concat, this,
+                         [getArgs(1), getArgs(2)], [1, 2]);
+
+  Testing.ExpectFunction('Array.concat', Array.ConcatSafe, this,
+                         [getArgs(), getArgs()], []);
+  Testing.ExpectFunction('Array.concat', Array.ConcatSafe, this,
+                         [getArgs(1), getArgs()], [1]);
+  Testing.ExpectFunction('Array.concat', Array.ConcatSafe, this,
+                         [getArgs(1, 2), getArgs()], [1, 2]);
+  Testing.ExpectFunction('Array.concat', Array.ConcatSafe, this,
                          [getArgs(1), getArgs(2)], [1, 2]);
 });
 
