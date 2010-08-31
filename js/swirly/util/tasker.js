297f4e7cdb3cc0b9eaa0c85e8b97d592a62447c3
Tom Swirly <tom@swirly.com>
2010-08-31 16:25:08 -0400

    Added some documentation.

--- js/swirly/util/tasker.js
+++ js/swirly/util/tasker.js
@@ -4,8 +4,34 @@
 // Tasker is a  class with a single method, Run, that applies the given function
 // with the given "this" and args.
 //
-// Tasker works around the issue documented in
-// http://tinyurl.com/max-js-task-destroys-closures.
+// Tasker works around the issue documented in http://ax.to/jstasks
+//
+// Here's how you'd use it!
+//
+// Typically, you would want to use a closure in a Task callback in the
+// following manner:
+//
+//   var a = DoSomething();
+//   var b = DoSomethingElse();
+//   var task = new Task(function() { DoMyTask(a, b); });
+//   task.schedule(1000);  // Do this in one second.
+//
+// Unfortunately, due to the bug described above, this does not work - when the
+// function DoMyTask is executed, a and b are undefined.
+//
+// The Tasker class fixes this problem:
+//
+//   var a = DoSomething();
+//   var b = DoSomethingElse();
+//   var task = new Tasker(this, DoMyTask, [a, b]);
+//   task.Schedule(1000);
+//
+// Please note that as I write this, I realize that I have only written this
+// Tasker class to work well with "one-shot" tasks - tasks that happen once and
+// not again - and not tasks that use "repeat".
+//
+// It will be pretty easy to fix it to be more general, and I'll do this soon.
+
 function Tasker(object, method, args) {
   this.object = object;
   this.method = method;
