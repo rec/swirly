7867e242fcd396a6919bca7c5d1d3bbaebb5acd7
Tom Swirly <tom@swirly.com>
2012-02-04 19:02:44 -0500

    Changes to the Commander

--- js/swirly/live/getTrackName.js
+++ js/swirly/live/getTrackName.js
@@ -7,4 +7,13 @@ Live.getTrackName = function() {
   return new LiveAPI('this_device canonical_parent').get('name');
 };
 
+Live.onTrackName = function(callback) {
+  var live;
+  function localCallback() {
+    callback(live.get('name'));
+  };
+  var live = new LiveAPI(localCallback, 'this_device canonical_parent');
+  live.property = 'name';
+};
+
 #endif  // __GETTRACKNAME
