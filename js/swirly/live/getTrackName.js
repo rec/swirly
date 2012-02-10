a8fb85b33c50f1560a29f829b8d40a0d8ae72703
Tom Swirly <tom@swirly.com>
2012-02-10 18:06:16 -0500

    Implemented song field.

--- js/swirly/live/getTrackName.js
+++ js/swirly/live/getTrackName.js
@@ -2,6 +2,7 @@
 #define __GETTRACKNAME
 
 #include "swirly/live/live.js"
+#include "swirly/util/is_string.js"
 
 Live.getTrackName = function() {
   return new LiveAPI('this_device canonical_parent').get('name');
@@ -10,7 +11,10 @@ Live.getTrackName = function() {
 Live.onTrackName = function(callback) {
   var live;
   function localCallback() {
-    callback(live.get('name'));
+    var name = live.get('name');
+    if (!Util.IsString(name))
+      name = name[0];
+    callback(name);
   };
   var live = new LiveAPI(localCallback, 'this_device canonical_parent');
   live.property = 'name';
