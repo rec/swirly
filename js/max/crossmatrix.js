56647aea4f4354484bdd4faee96b387b02b51fe2
Tom Ritchford <tom@swirly.com>
2015-03-09 00:19:23 -0400

    More crossmatrix changes.

--- js/max/crossmatrix.js
+++ js/max/crossmatrix.js
@@ -10,68 +10,83 @@ inlets = 1;
 outlets = 1;
 autowatch = 1;
 
-// global variables
-var ncols = 4; // default columns
-var nrows = 4; // default rows
+function Matrix(config) {
+    config = config || this.default_config;
 
-var vbrgb = [0.8, 1., 0.8, 0.5];
-var vmrgb = [0.9, 0.5, 0.5, 0.75];
-var vfrgb = [1.0, 0.0,  0.2, 1.0];
+    for (var name in config)
+        this[name] = config[name];
 
-// initialize state array
-var state = new Array(8);
+    this.state = new Array(this.columns);
 
-for (var i = 0; i < 8; i++)
-{
-	state[i] = new Array(8);
-	for (var j = 0; j < 64; j++)
-		state[i][j] = 0;
-}
+    for (var c = 0; c < this.columns; ++c)
+        this.state[c] = new Array(this.rows);
+};
+
+Matrix.prototype.clear = function() {
+    for (var c = 0; c < this.columns; ++c)
+        for (var r = 0; r < this.rows; ++r)
+            this.state[c][r] = 0;
+};
+
+Matrix.prototype.default_config = {
+    rows: 4,
+    columns: 4,
+    color: {
+        background: [0.8, 1.0, 0.8, 0.5],
+        disabled: [0.9, 0.5, 0.5, 0.75],
+        enabled: [1.0, 0.0, 0.2, 1.0]
+    }
+};
+
+Matrix.prototype.draw = function() {
+	with (sketch)
+	{
+		// set how the polygons are rendered
+        var vbrgb = this.color.background,
+            vfrgb = this.color.enabled,
+            vmrgb = this.color.disabled;
+		glclearcolor(vbrgb[0], vbrgb[1], vbrgb[2], vbrgb[3]); // set the clear color
+		glclear(); // erase the background
+		colstep = 2.0 / this.columns; // how much to move over per column
+		rowstep = 2.0 / this.rows; // how much to move over per row
+		for (var i = 0; i < this.columns; i++) // iterate through the columns
+		{
+			for (var j = 0; j < this.rows; j++) // iterate through the rows
+			{
+				moveto((i*colstep + colstep/2)-1.0,
+                       1.0 - (j*rowstep + rowstep/2), 0.); // move the drawing point
+				if (this.state[i][j]) // set 'on' color
+					glcolor(vfrgb[0], vfrgb[1], vfrgb[2], vfrgb[3]);
+				else // set 'off' color (midway between vbrgb and vfrgb)
+					glcolor(vmrgb[0], vmrgb[1], vmrgb[2], vmrgb[3]);
+				circle(0.7 / Math.max(this.rows, this.columns)); // draw the circle
+			}
+		}
+	}
+    refresh();
+};
+
+var matrix = new Matrix();
 
 // set up jsui defaults to 2d
 sketch.default2d();
 
 // initialize graphics
-draw();
+matrix.draw();
 
-// draw -- main graphics function
-function draw()
-{
-	with (sketch)
-	{
-		// set how the polygons are rendered
-		glclearcolor(vbrgb[0], vbrgb[1], vbrgb[2], vbrgb[3]); // set the clear color
-		glclear(); // erase the background
-		colstep = 2.0 / ncols; // how much to move over per column
-		rowstep = 2.0 / nrows; // how much to move over per row
-		for (var i = 0; i < ncols; i++) // iterate through the columns
-		{
-			for(var j = 0; j < nrows; j++) // iterate through the rows
-			{
-				moveto((i*colstep + colstep/2)-1.0,
-                       1.0 - (j*rowstep + rowstep/2), 0.); // move the drawing point
-				if (state[i][j]) // set 'on' color
-					glcolor(vfrgb[0], vfrgb[1], vfrgb[2], vfrgb[3]);
-				else // set 'off' color (midway between vbrgb and vfrgb)
-					glcolor(vmrgb[0], vmrgb[1], vmrgb[2], vmrgb[3]);
-				circle(0.7 / Math.max(nrows, ncols)); // draw the circle
-			}
-		}
-	}
-    refresh();
-}
-
-bang = draw;
+function bang() {
+    matrix.draw();
+};
 
 // rows -- change number of rows in jsui
 function rows(val)
 {
 	if (arguments.length)
 	{
-		nrows = arguments[0];
-		draw(); // draw and refresh display
+		matrix.rows = arguments[0];
+		matrix.draw(); // draw and refresh display
 	}
-}
+};
 
 
 // cols -- change number of columns is jsui
@@ -79,31 +94,31 @@ function cols(val)
 {
 	if (arguments.length)
 	{
-		ncols = arguments[0];
-		draw(); // draw and refresh display
+		matrix.columns = arguments[0];
+		matrix.draw(); // draw and refresh display
 	}
-}
+};
 
 // list -- update our state to respond to a change from Max
 function list(v)
 {
 	if (arguments.length == 3) // bail if incorrect number of arguments
 	{
-		state[arguments[0]][arguments[1]] = arguments[2]; // update our internal state based on the list
+		matrix.state[arguments[0]][arguments[1]] = arguments[2]; // update our internal state based on the list
 		outlet(0, arguments[0], arguments[1], arguments[2]); // echo the list out the outlet
 	}
-	draw(); // draw and refresh display
-}
+	matrix.draw(); // draw and refresh display
+};
 
 // clear -- wipe the state clean
 function clear()
 {
-	for (var i = 0; i < ncols; i++)
-		for (var j = 0; j < nrows; j++)
-			state[i][j] = 0; // wipe the state
+	for (var i = 0; i < matrix.columns; i++)
+		for (var j = 0; j < matrix.rows; j++)
+			matrix.state[i][j] = 0; // wipe the state
 	outlet(0, "clear"); // clear the router or matrix~ downstream
-	draw(); // draw and refresh display
-}
+	matrix.draw(); // draw and refresh display
+};
 
 // frgb -- change foreground (clicked) sphere color
 function frgb(r,g,b)
@@ -114,8 +129,8 @@ function frgb(r,g,b)
 	vmrgb[0] = 0.5 * (vfrgb[0] + vbrgb[0]);
 	vmrgb[1] = 0.5 * (vfrgb[1] + vbrgb[1]);
 	vmrgb[2] = 0.5 * (vfrgb[2] + vbrgb[2]);
-	draw(); // draw and refresh display
-}
+	matrix.draw(); // draw and refresh display
+};
 
 // brgb -- change background color
 function brgb(r,g,b)
@@ -126,41 +141,39 @@ function brgb(r,g,b)
 	vmrgb[0] = 0.5 * (vfrgb[0] + vbrgb[0]);
 	vmrgb[1] = 0.5 * (vfrgb[1] + vbrgb[1]);
 	vmrgb[2] = 0.5 * (vfrgb[2] + vbrgb[2]);
-	draw(); // draw and refresh display
-}
+	matrix.draw(); // draw and refresh display
+};
 
 // onresize -- deal with a resized jsui box
 function onresize(w,h)
 {
-	draw(); // draw and refresh display
-}
-
-onresize.local = 1; // make function private to prevent triggering from Max
-
+	matrix.draw(); // draw and refresh display
+};
 
 // onclick -- deal with mouse click event
 function onclick(x,y)
 {
-	worldx = sketch.screentoworld(x, y)[0];
-	worldy = sketch.screentoworld(x, y)[1];
+	var worldx = sketch.screentoworld(x, y)[0];
+	var worldy = sketch.screentoworld(x, y)[1];
 
-	colwidth = 2. / ncols; // width of a column, in world coordinates
-	rowheight = 2. / nrows; // width of a row, in world coordinates
+	var colwidth = 2. / matrix.columns; // width of a column, in world coordinates
+	var rowheight = 2. / matrix.rows; // width of a row, in world coordinates
 
-	x_click = Math.floor((worldx + 1.0) / colwidth); // which column we clicked
-	y_click = Math.floor((1.0 - worldy) / rowheight); // which row we clicked
+	var x_click = Math.floor((worldx + 1.0) / colwidth); // which column we clicked
+	var y_click = Math.floor((1.0 - worldy) / rowheight); // which row we clicked
 
-	state[x_click][y_click] = !state[x_click][y_click]; // flip the state of the clicked point
-	outlet(0, x_click, y_click, state[x_click][y_click]); // output the coordinates and state of the clicked point
+	matrix.state[x_click][y_click] = !matrix.state[x_click][y_click]; // flip the state of the clicked point
+	outlet(0, x_click, y_click, matrix.state[x_click][y_click]); // output the coordinates and state of the clicked point
 
-	draw(); // draw and refresh display
-}
-onclick.local = 1; // make function private to prevent triggering from Max
+	matrix.draw(); // draw and refresh display
+};
 
 // ondblclick -- pass buck to onclick()
 function ondblclick(x, y)
 {
 	onclick(x, y);
-}
+};
 
+onclick.local = 1; // make function private to prevent triggering from Max
 ondblclick.local = 1; // make function private to prevent triggering from Max
+onresize.local = 1; // make function private to prevent triggering from Max
