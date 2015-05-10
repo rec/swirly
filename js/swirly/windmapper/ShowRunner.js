4e335db5a671bc76ec8f8fc83280498123fecc19
Tom Ritchford <tom@swirly.com>
2015-05-10 15:35:50 -0400

    new load function that loads a new state from a file.

--- js/swirly/windmapper/ShowRunner.js
+++ js/swirly/windmapper/ShowRunner.js
@@ -3,8 +3,22 @@
 #include "swirly/max/findObjects.js"
 #include "swirly/util/print.js"
 
+function DefaultScene() {
+    this.transport = function() {};
+    this.note = function() {};
+    this.breath = function() {};
+    this.program = function() {};
+    this.pitchbend = function() {};
+    this.level = function() {};
+    this.phasor = function() {};
+    this.timer = function() {};
+
+    this.load = function(fname) {
+        post('loading ', fname, '\n');
+    };
+};
+
 function ShowRunner() {
-    var that = this;
     this._help = {
         'transport': 'A list representing the transport number.',
         'note': 'MIDI note on and off',
@@ -14,24 +28,24 @@ function ShowRunner() {
         'level': 'audio level',
         'phasor': 'The phasor value between 0 and 1',
         'timer': 'Result of some incoming time',
+        'load': 'Load a scene from a disk file.'
     };
 
-    function fromProxy(name) {
+    this.scene = new DefaultScene();
+
+    var self = this;
+    function fromScene(name) {
         return function(_) {
-            if (!that.proxy) {
-                post('ERROR: no proxy (method=', name, ')\n');
-            } else {
-                var method = that.proxy[fname];
-                if (method)
-                    method.apply(this, arrayfromargs(arguments));
-                else
-                    post('ERROR: no proxy method for name=', name, '\n');
-            }
+            var method = self.scene[name];
+            if (method)
+                method.apply(self, arrayfromargs(arguments));
+            else
+                post('ERROR: no proxy method for name=', name, '\n');
         };
     };
 
     for (var name in this._help)
-        this[name] = fromProxy(name);
+        this[name] = fromScene(name);
 
     this.objects = Max.findAll();
 };
