cfb3497f02bf38256317b10e828e056054bf2a92
Tom Ritchford <tom@swirly.com>
2015-05-24 20:19:23 -0400

    More on Instrument.js

--- js/swirly/windmapper/Instrument.js
+++ js/swirly/windmapper/Instrument.js
@@ -4,13 +4,17 @@
 
 function Instrument(channels, start, names, defaults) {
     var nameToChannel = Util.invertArray(channels),
-        self = this;
+        self = this,
+        defaultValues = {};
+
+    for (var i = 0; i < channels.length; ++i)
+        defaultValues[i] = defaults[channels[i]] || 0;
 
     this.channel = function(channel) {
         var chan = nameToChannel[channel];
         if (chan === undefined)
             throw 'Instrument.channel: Don\'t understand channel ' + name;
-        return chan;
+        return chan + start;
     };
 
     this.value = function(channel, value) {
@@ -27,9 +31,9 @@ function Instrument(channels, start, names, defaults) {
     };
 
     this.remap = function(dict) {
-        var result = {};
-        for (var d in dict)
-            result[self.channel(d)] = self.value(dict[d]);
+        var result = Util.Dict.Copy(defaultValues);
+        for (var channel in dict)
+            result[self.channel(channel)] = self.value(dict[channel]);
         return result;
     };
 };
