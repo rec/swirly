f77a1f810dcebf667cfb62c1876b245a89d146b7
Tom Ritchford <tom@swirly.com>
2015-06-06 15:59:47 -0400

    New scene format code.

--- js/swirly/util/ExpandCommandJson.js
+++ js/swirly/util/ExpandCommandJson.js
@@ -3,27 +3,50 @@
 #include "swirly/util/util.js"
 
 Util.expandCommandJSON = function(json, commandTable) {
+    function applyCommand(name, args) {
+        var command = commandTable[name.slice(1)];
+        if (!command)
+            throw 'Don\'t understand command ' + name;
+        return command.apply(this, args || []);
+    };
+
     function expand(json) {
-        if (!(json instanceof Object))
-            return json;
         if (json instanceof Array) {
+            if (json[0] && json[0][0] == '*')
+                return applyCommand(json[0], json.slice(1));
             var result = [];
             for (var i = 0; i < json.length; ++i)
                 result.push(expand(json[i]));
             return result;
         }
-        if (json.command) {
-            var command = commandTable[json.command];
-            if (!command)
-                throw 'Couldn\'t understand command ' + json.command;
-            return command(json.data);
+
+        if (json instanceof Object) {
+            var result = {};
+            for (var i in json)
+                result[i] = expand(json[i]);
+            return result;
         }
 
-        var result = {};
-        for (var i in json)
-            result[i] = expand(json[i]);
-        return result;
+        if (typeof(json) === 'string' && json[0] === '*')
+            return applyCommand(json);
+
+        return json;
     };
 
     return expand(json);
+
+};
+
+/** Expand a string address*/
+Util.getFromAddress = function(dict, address) {
+    if (typeof(address) === 'string')
+        address = address.split('.');
+
+    var result = dict;
+    for (var i = 0; i < address.length; ++i) {
+        result = result[address[i]];
+        if (!result)
+            throw 'Can\'t get address ' + address[i];
+    }
+    return result;
 };
