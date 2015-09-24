7e6e2d08a2f720dbd60fa215cb242dc77383e4bf
Tom Ritchford <tom@swirly.com>
2015-09-24 13:25:10 -0400

    More Consolidate.

--- js/swirly/scene/SceneMaker.js
+++ js/swirly/scene/SceneMaker.js
@@ -3,29 +3,27 @@
 #include "swirly/scene/Scene.js"
 #include "swirly/show/VLProgram.js"
 
-Scene.channel = function(name) {
-    return function(show, args) {
-        return Scene.makeEach(show, args, {
-            mute: Scene.setter(name, 'mute'),
-            level: Scene.setter(name, 'level'),
-        });
-    };
-};
-
 /** Every scene is is a function that returns either a pure function or an array
     of functions. */
 Scene.make = function(show) {
-    var makers = {
-        mic: Scene.channel('mic'),
-        vl70: Scene.channel('vl70'),
-        program: VL.makeScene,
-        lights: Lights.makeScene,
-        processor: Processor.makeScene,
-        tempo: Live.makeTempoScene,
+    function channel(name) {
+        return function(show, args) {
+            return Scene.makeEach(show, args, {
+                mute: Scene.setter(name, 'mute'),
+                level: Scene.setter(name, 'level'),
+            });
+        };
     };
 
     return applyEachObj(show.json.scenes, function(args, name) {
-        var scenes = Scene.makeEach(show, args, makers);
+        var scenes = Scene.makeEach(show, args, {
+            mic: channel('mic'),
+            vl70: channel('vl70'),
+            program: VL.makeScene,
+            lights: Lights.makeScene,
+            processor: Processor.makeScene,
+            tempo: Live.makeTempoScene,
+        });
         return Dict.sequence(Dict.flatten(scenes));
     });
 };
